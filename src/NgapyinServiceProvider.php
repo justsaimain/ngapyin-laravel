1450dc4522df49a5e983c825ca1658a483161083
justsaimain <saimain.primary@gmail.com>
2021-12-01 22:54:55 +0630

    🐞 migrations

--- src/NgapyinServiceProvider.php
+++ src/NgapyinServiceProvider.php
@@ -12,35 +12,37 @@ class NgapyinServiceProvider extends ServiceProvider
 
     public function boot(Kernel $kernel)
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadViewsFrom(__DIR__.'/resources/views','admin_login');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'admin_login');
 
 
         if ($this->app->runningInConsole()) {
 
             $this->publishes([
-              __DIR__.'/config/auth.php' => config_path('auth.php'),
+                __DIR__ . '/config/auth.php' => config_path('auth.php'),
             ], 'auth');
 
             $this->publishes([
-                __DIR__.'/assets' => public_path('vendor/assets'),
-              ], 'assets');
+                __DIR__ . '/assets' => public_path('vendor/assets'),
+            ], 'assets');
+        }
 
-          }
+        if ($this->app->runningInConsole()) {
 
-          if ($this->app->runningInConsole()) {
+            // Export the migration
+            $this->publishes([
+                __DIR__ . 'src/database/migrations/create_admins_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_admins_table.php'),
+                // you can add any number of migrations here
+            ], 'migrations');
             $this->commands([
                 CreateAdmin::class,
             ]);
         }
-
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/config/auth.php','auth');
+        $this->mergeConfigFrom(__DIR__ . '/config/auth.php', 'auth');
     }
-
-
 }
